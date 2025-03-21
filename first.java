0c234814fb856bcd8ee6403bc01147d76eb7555d
THANUJA12345379 <thanuthanujam2002@gmail.com>
2025-03-21 14:53:37 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,20 @@
+package javaopps;
+
+public class first {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a<b);
+		System.out.println(a<<2);
+		System.out.println(a>>2);
+	}
+
+}
